6fee09282780ae3cd05ee5d6e04209bd70aecc24
lekszay <lekszay@gmail.com>
2023-08-16 10:45:29 +0300

    Variables in Rust.

---
+++ variables/target/debug/deps/variables-af3b7adf1b067932.d
@@ -0,0 +1,5 @@
+/home/lekszay/Desktop/Rust/LearningRust/variables/target/debug/deps/variables-af3b7adf1b067932: src/main.rs
+
+/home/lekszay/Desktop/Rust/LearningRust/variables/target/debug/deps/variables-af3b7adf1b067932.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/lekszay/Desktop/Rust/LearningRust/variables/target/debug/variables: /home/lekszay/Desktop/Rust/LearningRust/variables/src/main.rs
